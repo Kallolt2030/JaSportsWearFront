cbf3d04e80b683f7912d1752afbb805e32040c6e
pipoyt <pipokueryt@gmail.com>
2026-07-26 14:23:50 -0600

    CARRITO1

--- src/app/component/public/carrito/carrito.component.spec.ts
+++ src/app/component/public/carrito/carrito.component.spec.ts
@@ -1,23 +1,122 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
-import { CarritoComponent } from './carrito.component';
+@Injectable({
+  providedIn: 'root'
+})
+export class CarritoService {
 
-describe('CarritoComponent', () => {
-  let component: CarritoComponent;
-  let fixture: ComponentFixture<CarritoComponent>;
+  private carrito: any[] = [];
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [CarritoComponent]
-    })
-    .compileComponents();
+  private carritoSubject = new BehaviorSubject<any[]>([]);
 
-    fixture = TestBed.createComponent(CarritoComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+  carrito$ = this.carritoSubject.asObservable();
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+  constructor() {
+    this.cargarCarrito();
+  }
+
+  private cargarCarrito() {
+    const datos = localStorage.getItem('carrito');
+
+    if (datos) {
+      this.carrito = JSON.parse(datos);
+      this.carritoSubject.next(this.carrito);
+    }
+  }
+
+  private guardarCarrito() {
+    localStorage.setItem('carrito', JSON.stringify(this.carrito));
+    this.carritoSubject.next(this.carrito);
+  }
+
+  agregarProducto(producto: any) {
+
+    const existe = this.carrito.find(p => p.id === producto.id);
+
+    if (existe) {
+      existe.quantity++;
+    } else {
+
+      this.carrito.push({
+        ...producto,
+        quantity: 1
+      });
+
+    }
+
+    this.guardarCarrito();
+  }
+
+  obtenerProductos() {
+    return this.carrito;
+  }
+
+  aumentarCantidad(id: number) {
+
+    const producto = this.carrito.find(p => p.id === id);
+
+    if (producto) {
+      producto.quantity++;
+      this.guardarCarrito();
+    }
+
+  }
+
+  disminuirCantidad(id: number) {
+
+    const producto = this.carrito.find(p => p.id === id);
+
+    if (!producto) return;
+
+    if (producto.quantity > 1) {
+
+      producto.quantity--;
+
+    } else {
+
+      this.eliminarProducto(id);
+
+    }
+
+    this.guardarCarrito();
+
+  }
+
+  eliminarProducto(id: number) {
+
+    this.carrito = this.carrito.filter(p => p.id !== id);
+
+    this.guardarCarrito();
+
+  }
+
+  vaciarCarrito() {
+
+    this.carrito = [];
+
+    this.guardarCarrito();
+
+  }
+
+  getTotal(): number {
+
+    return this.carrito.reduce((total, producto) => {
+
+      return total + (producto.price * producto.quantity);
+
+    }, 0);
+
+  }
+
+  getCantidadProductos(): number {
+
+    return this.carrito.reduce((total, producto) => {
+
+      return total + producto.quantity;
+
+    }, 0);
+
+  }
+
+}
